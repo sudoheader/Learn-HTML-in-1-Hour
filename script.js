fd6fa92724db714d264afec6181e35f6491b53f3
sudoheader <sudoheader@users.noreply.github.com>
2017-08-19 20:56:09 -0700

    40. Variable Scope

--- script.js
+++ script.js
@@ -8,3 +8,7 @@ function changeTheText() {
     var changeTextVariable = document.getElementById('changeUserText').value;
     document.getElementById('paragraphText').innerHTML = changeTextVariable;
 }
+
+function fun() {
+
+}
