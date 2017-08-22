5239716e2cd972bd42f280b823e2896c4ec084cb
sudoheader <sudoheader@users.noreply.github.com>
2017-08-21 22:07:34 -0700

    47. Changing the style tags with JavaScript

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
 var coolNumber = 3;
 
 function changeTheText() {
-
-    if (coolNumber == 1) {
-        var changeTextVariable = document.getElementById('changeUserText').value;
-        document.getElementById('paragraphText').innerHTML = changeTextVariable;
-    } else if (coolNumber == 2) {
-        alert("pop");
-    } else if (coolNumber == 3) {
-        alert("Awesome");
-    } else {
-        var changeTextVariable1 = "Else statem fired";
-        document.getElementById('paragraphText').innerHTML = changeTextVariable1;
-    }
+    document.getElementById('paragraphText').style.color = 'orange';
+    var changeTextVariable = document.getElementById('changeUserText').value;
+    document.getElementById('paragraphText').innerHTML = changeTextVariable;
 }
