c48f0c6362e18078d5c3bf566f35755200725fa0
sudoheader <sudoheader@users.noreply.github.com>
2017-08-21 21:56:26 -0700

    44. Else statements

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-var coolNumber = 1;
+var coolNumber = 2;
 
 function changeTheText() {
 
     if (coolNumber == 1) {
         var changeTextVariable = document.getElementById('changeUserText').value;
+        document.getElementById('paragraphText').innerHTML = changeTextVariable;
+    }
+    else {
+        var changeTextVariable = "Else statem fired";
         document.getElementById('paragraphText').innerHTML = changeTextVariable;
     }
 }
