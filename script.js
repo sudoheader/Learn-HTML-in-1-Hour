3dc948276e74a5a2ca37279320a093c0e59cd997
sudoheader <sudoheader@users.noreply.github.com>
2017-08-23 14:59:42 -0700

    54. Adding in a second button

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
     //(HTML object). action (do something)
-    $("button").mouseleave(function() {
+    $("#button1").click(function() {
         //(HTML object). action
         $("h1").fadeOut();
         $("h2").fadeOut();
+    });
+    $("#button2").click(function() {
+        $("h1").fadeIn();
+        $("h2").fadeIn();
     });
 });
