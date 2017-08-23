e911ddb10a9f7ebc954acac9ed73d46aecae6149
sudoheader <sudoheader@users.noreply.github.com>
2017-08-23 15:06:48 -0700

    56. Adding classes to different HTML tags

--- script.js
+++ script.js
@@ -2,11 +2,9 @@ $(document).ready(function() {
     //(HTML object). action (do something)
     $("#button1").click(function() {
         //(HTML object). action
-        $("h1").fadeOut();
-        $("h2").fadeOut();
+        $(".fadeouttag").fadeOut();
     });
     $(".otherbutton").click(function() {
-        $("h1").fadeIn();
-        $("h2").fadeIn();
+        $(".fadeouttag").fadeIn();
     });
 });
