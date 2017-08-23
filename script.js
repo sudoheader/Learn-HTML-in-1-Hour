3086a66cd6462352855931db2d82c68e0acc1510
sudoheader <sudoheader@users.noreply.github.com>
2017-08-22 23:26:44 -0700

    53. More events you can use

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
-    $("button").click(function() {
+    //(HTML object). action (do something)
+    $("button").mouseleave(function() {
+        //(HTML object). action
         $("h1").fadeOut();
         $("h2").fadeOut();
     });
