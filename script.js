d75cc45d56d08647e5212cde85d580f2f3e79ee3
sudoheader <sudoheader@users.noreply.github.com>
2017-08-22 17:16:40 -0700

    48. jQuery Library file

--- script.js
+++
@@ -1,7 +0,0 @@
-var coolNumber = 3;
-
-function changeTheText() {
-    document.getElementById('paragraphText').style.color = 'orange';
-    var changeTextVariable = document.getElementById('changeUserText').value;
-    document.getElementById('paragraphText').innerHTML = changeTextVariable;
-}
