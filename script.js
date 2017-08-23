8c1399c2d975d93e426ef40cbc1784488c8e09f8
sudoheader <sudoheader@users.noreply.github.com>
2017-08-23 15:11:22 -0700

    57. Fade out timing

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ $(document).ready(function() {
     //(HTML object). action (do something)
     $("#button1").click(function() {
         //(HTML object). action
-        $(".fadeouttag").fadeOut();
+        // $(".fadeouttag").fadeOut();
+        $("h1").fadeOut();
+        $("h2").fadeOut();
+
     });
     $(".otherbutton").click(function() {
-        $(".fadeouttag").fadeIn();
+        $(".fadeouttag").fadeIn(3000);
     });
 });
