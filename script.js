8932e1dabdc91080bd0dbac46d82fb09e3edc4d5
sudoheader <sudoheader@users.noreply.github.com>
2017-08-23 15:02:51 -0700

    55. Id and class

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
         $("h1").fadeOut();
         $("h2").fadeOut();
     });
-    $("#button2").click(function() {
+    $(".otherbutton").click(function() {
         $("h1").fadeIn();
         $("h2").fadeIn();
     });
