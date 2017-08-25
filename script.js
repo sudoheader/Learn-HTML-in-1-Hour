8653955afd66481f102d350917851b02f398930b
sudoheader <sudoheader@users.noreply.github.com>
2017-08-24 18:47:50 -0700

    69. Div tags

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 $(document).ready(function() {
     //(HTML object). action (do something)
     $("#button1").click(function() {
-        $("div").animate({left: '100px'}, "slow");
+        $("#orangediv").animate({height: '250px'});
     });
     $("#button2").click(function() {
-        $("div").animate({left: '0px'}, "fast");
-    });
-    $("#button3").click(function() {
-        $("div").animate({height: '+=20px', width: '+=20px'}, "fast");
-    });
-    $("#button4").click(function() {
-        $("div").animate({height: '-=10px', width: '-=10px'}, "fast");
+        $("#bluediv").animate({height: '250px'});
     });
 });
