6f19a01e5f162be599f6fb229e2d78673fc8e649
sudoheader <sudoheader@users.noreply.github.com>
2017-08-24 18:31:12 -0700

    65. Sliding slow

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
     //(HTML object). action (do something)
     $("#button1").click(function() {
-        $("div").animate({left: '100px'});
+        $("div").animate({left: '100px'}, "slow");
     });
     $("#button2").click(function() {
-        $("div").animate({left: '0px'});
+        $("div").animate({left: '0px'}, "fast");
     });
 });
