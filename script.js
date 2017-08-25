0cfb6984613215ccb243380064c6f193781c4ef1
sudoheader <sudoheader@users.noreply.github.com>
2017-08-24 18:20:03 -0700

    61. Hide and show

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
     //(HTML object). action (do something)
     $("#button1").click(function() {
-        //(HTML object). action
-        // $(".fadeouttag").fadeOut();
-        $("p:first").fadeOut();
+        $(".fadeouttag").hide();
+    });
+    $("#button2").click(function() {
+        $(".fadeouttag").show();
     });
 });
