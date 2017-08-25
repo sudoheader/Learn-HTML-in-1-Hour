8b444d33059132eaa897528464a24f43df2e17f2
sudoheader <sudoheader@users.noreply.github.com>
2017-08-24 18:34:38 -0700

    66. Growing a div tag

--- script.js
+++ script.js
@@ -6,4 +6,7 @@ $(document).ready(function() {
     $("#button2").click(function() {
         $("div").animate({left: '0px'}, "fast");
     });
+    $("#button3").click(function() {
+        $("div").animate({height: '200px', width: '200px'}, "fast");
+    });
 });
