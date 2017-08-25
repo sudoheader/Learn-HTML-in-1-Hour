af31ad60764e30c9557405aa2a6ff824a505d155
sudoheader <sudoheader@users.noreply.github.com>
2017-08-24 18:26:59 -0700

    63. Animation

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 $(document).ready(function() {
     //(HTML object). action (do something)
     $("#button1").click(function() {
-        $(".fadeouttag").hide();
-    });
-    $("#button2").click(function() {
-        $(".fadeouttag").show();
+        $("div").animate({left: '100px'});
     });
 });
