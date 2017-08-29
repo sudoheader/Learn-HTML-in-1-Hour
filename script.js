65ff1a149e4f99bd3dc5133b819584a636867891
sudoheader <sudoheader@users.noreply.github.com>
2017-08-28 22:11:55 -0700

    79. Circular gradients

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 var aCanvas = document.getElementById("aCanvas"));
 var context = aCanvas.getContext("2d");
+var aGradient = context.createRadialGradient(10, 10, 3, 50, 50, 70);
+aGradient.addColorStop(0, "orange");
+aGradient.addColorStop(1,"blue");
 context.fillStyle = "red";
-context.fillRect(0,0,150,150);
+context.fillRect(0, 0, 150, 150);
