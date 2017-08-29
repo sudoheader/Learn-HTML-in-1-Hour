8f25f8de35bfb363301a34d6feaff0a61073792b
sudoheader <sudoheader@users.noreply.github.com>
2017-08-28 23:14:02 -0700

    80. Linear gradients

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var aCanvas = document.getElementById("aCanvas"));
 var context = aCanvas.getContext("2d");
-var aGradient = context.createRadialGradient(10, 10, 3, 50, 50, 70);
+var aGradient = context.createLinearGradient(10, 11, 100, 0);
 aGradient.addColorStop(0, "orange");
-aGradient.addColorStop(1,"blue");
-context.fillStyle = "red";
+aGradient.addColorStop(1, "blue");
+context.fillStyle = aGradient;
 context.fillRect(0, 0, 150, 150);
