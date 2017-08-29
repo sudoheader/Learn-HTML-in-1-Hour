34dcd30752a3935c51c881a2dc99aaac4d32533c
sudoheader <sudoheader@users.noreply.github.com>
2017-08-29 08:48:16 -0700

    82. Drawing a circle on the canvas

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 var aCanvas = document.getElementById("aCanvas");
 var context = aCanvas.getContext("2d");
-var aGradient = context.createLinearGradient(10, 11, 100, 0);
-context.shadowBlur = 10;
-context.shadowColor = "black";
-aGradient.addColorStop(0, "orange");
-aGradient.addColorStop(1, "blue");
-context.fillStyle = aGradient;
-context.fillRect(0, 0, 150, 150);
+context.beginPath();
+context.arc(100, 100, 50, 0, 2 * Math.PI);
+context.stroke();
