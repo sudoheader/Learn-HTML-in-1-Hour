301e5dcd44c40ac0ffd986845d6052199151b8bc
sudoheader <sudoheader@users.noreply.github.com>
2017-08-28 20:44:22 -0700

    78. JavaScript HTML 5 functions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 var aCanvas = document.getElementById("aCanvas"));
 var context = aCanvas.getContext("2d");
 context.fillStyle = "red";
-context.fillRect(10,10,100,100);
+context.fillRect(0,0,150,150);
